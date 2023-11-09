bbb6650ecee79db4052c0185fd834da9d4bf9ac0
Faysa Ryestitha Etsuko <etsukofr21d@student.unhas.ac.id>
2023-11-09 15:06:02 +0800

    styles: memberikan styles ke page utama

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,21 @@
 import Image from 'next/image'
+import './globals.css'
 
 export default function Home() {
   return (
-    <div>
+    <div className="body">
       <div className="banner-container">
         <div className="header-banner-wrapper">
           <div className="profile-header-banner">
-          <Image
-            src="/assets/account.png"
-            width={500}
-            height={500}
-            alt="Picture of the author"
-          />
+            <Image
+              src="/assets/account.png"
+              alt="Picture of the author"
+              fill
+              objectFit='contain'
+            />
           </div>
           <div className="content-header-banner">
-            <h1>Faysa Ryestitha Etsuko</h1>
+            <h1>Faysa Ryestitha E.</h1>
             <div className="bio-nim-header-banner">
               <p>D121211027</p>
               <p>Do your best</p>
@@ -22,9 +23,11 @@ export default function Home() {
           </div>
         </div>
         <div className="cta-banner-wrapper">
-          <button>
-            Halo!
-          </button>
+          <a href=''>
+            <div className='cta-button'>
+              <p>Halo!</p>
+            </div>
+          </a>
         </div>
       </div>
     </div>
